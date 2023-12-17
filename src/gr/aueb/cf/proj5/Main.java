22317952a92168c927b8dfb403b0330b2ba912d0
Ina.Zenelaj <ina_zeneina@hotmail.com>
2023-12-17 20:21:48 +0200

    Main updated

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.proj5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello CF!!!");
+        System.out.println("Hello Coding Factory !!!!!!!!!!!!!");
     }
 }
